b575843228c252274ca9aef90cd62a049a694798
Josiah Arkson <34874253+josiaharkson@users.noreply.github.com>
2021-01-12 03:11:17 +0100

    Update index.js

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -21,7 +21,7 @@ const Home = () => {
             Records API
           </a>
         </h3>
-        <code>http://api.enye.tech/v1/challenge/records</code>
+        <code>https://api.enye.tech/v1/challenge/records</code>
 
         <Link to="/profiles/1" className={Styles.button}>
           <span>View All Profiles</span>
@@ -32,36 +32,4 @@ const Home = () => {
 };
 
 export default Home;
-
-// import React from "react";
-// import Styles from "./home.module.css";
-// import { Link } from "react-router-dom";
-// import useMediaQuery from "@material-ui/core/useMediaQuery";
-
-// const Home = () => {
-//   const matches = useMediaQuery("(min-width:768px)");
-
-//   return (
-//     <div className={Styles.root}>
-//       <main className={Styles.main}>
-//         <h1>Front-end Client</h1>
-//         <h2>
-//           List of all profiles from the <span>Records API</span>
-//         </h2>
-//         <code>http://api.enye.tech/v1/challenge/records</code>
-
-//         {matches ? (
-//           <img src="/guide_lg.PNG" alt="guide_lg" />
-//         ) : (
-//           <img src="/guide_sm.PNG" alt="guide_sm" />
-//         )}
-
-//         <Link to="/profiles/1" className={Styles.button}>
-//           <span>View All Profiles</span>
-//         </Link>
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default Home;
+ 
