346243299dfa51163ffb4e0062bcffc1377ce2da
Josiah Arkson <34874253+josiaharkson@users.noreply.github.com>
2021-01-12 02:52:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static("client/build"));
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 app.get("*", (request, response) => {
   response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
